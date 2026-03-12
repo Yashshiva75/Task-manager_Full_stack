3c23b4b8707b13d22c907aa283a52e6e11ae6b88
Yash shiva <124360143+Yashshiva75@users.noreply.github.com>
2026-03-12 22:24:26 +0530

    chages

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ require('dotenv').config();
 const router = express.Router();
 
 const app = express()
-app.use(cors());   
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 connectDB();
